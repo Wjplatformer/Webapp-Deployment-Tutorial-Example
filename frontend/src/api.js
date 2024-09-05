47613babdc68b2eb2e17b1c293af1ab836d05bdf
Wjplatformer <fongwenjun@hotmail.com>
2024-09-06 00:32:48 +0800

    add backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
 import { ACCES_TOKEN  } from './constants'
 
+const apiUrl = '/choreo-apis/django-react-tutorial/backend/v1'
+
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL //spefifies url to work with
-})
+    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl, //spefifies url to work with
+});
 
 api.interceptors.request.use( //Access token from backend and frontend etc
     (config) => {
